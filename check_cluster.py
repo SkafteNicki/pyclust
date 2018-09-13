690a8dc86c96cf153a24d7373853d20f53cd94a1
Nicki Skafte <skaftenicki@gmail.com>
2018-09-13 10:56:09 +0200

    function for checking which gpus are avaible

--- check_cluster.py
+++ check_cluster.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 12 11:58:56 2018
+Created on Wed Sep 12 14:42:44 2018
 
 @author: nsde
 """
@@ -15,28 +15,33 @@ if __name__ == '__main__':
     ssh.load_system_host_keys()
     
     try:
-        counter = 0
+        count_all = count_filt = 0
         for i in range(1, 13):
             print(47*"=" + "titan{0:02d}".format(i) + 47*"=")
-    
+            
             # Connect to host
             ssh.connect("titan{0:02d}".format(i))
-        
+            
             # Commands to run
-            comm = ['top -c -n 1 |grep nsde']
+            comm = [' cuda-smi']
             comm = ';'.join(comm)
-
+            
             # Execute commands        
             stdin, stdout, stderr = ssh.exec_command(comm, get_pty=True)
             
             # Print output
             output = stdout.read().decode("utf-8")
-            output = output.split('\n')
+            output = output.split('\n')[:-1]
+            
             for line in output:
-                if 'top' not in line and len(line)>1:
+                count_all += 1
+                in_use = int(line.split(':')[-1].split('of')[0])
+                if in_use < 20:
                     print(line)
-                    counter += 1
-        print('You are using ' + str(counter) + 'gpu')
+                    count_filt += 1
+        if count_all != count_filt:
+            print(101*"=")
+            print(str(count_filt) + ' availble GPUs out of ' + str(count_all))
+            
     finally:
         ssh.close()
-
