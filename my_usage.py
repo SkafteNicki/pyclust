8be377797f9c1281f629034009ee04b1564b790b
Nicki Skafte <skaftenicki@gmail.com>
2018-09-13 10:47:16 +0200

    check which gpus I use on the cluster

---
+++ my_usage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 12 11:58:56 2018
+
+@author: nsde
+"""
+
+#%%
+import paramiko
+
+#%%
+if __name__ == '__main__':
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    
+    try:
+        for i in range(1, 13):
+            print(47*"=" + "titan{0:02d}".format(i) + 47*"=")
+    
+            # Connect to host
+            ssh.connect("titan{0:02d}".format(i))
+        
+            # Commands to run
+            comm = ['top -c -n 1 |grep nsde']
+            comm = ';'.join(comm)
+
+            # Execute commands        
+            stdin, stdout, stderr = ssh.exec_command(comm, get_pty=True)
+            
+            # Print output
+            output = stdout.read().decode("utf-8")
+            output = output.split('\n')
+            for line in output:
+                if 'top' not in line and len(line)>1:
+                    print(line)
+    finally:
+        ssh.close()
+
